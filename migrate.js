fe69799d65088e895ac914db2707b9a8a9801961
Ahmad Nur Faizin <faizintenan@Gmail.com>
2025-05-20 23:11:27 +0700

    tes

--- migrate.js
+++ migrate.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  // Jangan tambah ssl atau lainnya, cukup ini saja
 });
 
 // const pool = new Pool({
